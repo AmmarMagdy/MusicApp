efef7a38ecdb0422aaa8113c38980c30fe0d7aff
ammar magdy <ammar.m.badawi@gmail.com>
2019-07-12 19:57:37 +0200

    Initial Commit

---
+++ Music App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music App
+//
+//  Created by Afnan on 7/12/19.
+//  Copyright © 2019 Afnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
